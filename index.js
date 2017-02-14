9b36189fad6875757ae324202e440e4bc01ddd34
swargo <sophie.junkmail@gmail.com>
2017-02-14 09:30:09 -0700

    index.pug and layout.pug

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ pug = require('pug'),
 path = require('path'),
 mods = require('./script.js');
 
-home = require('./routes/home.js'),
-menu = require('./routes/menu.js'),
-directions = require('./routes/directions.js');
- 
 var app = express(); 
  
 // use Pug and set the public folder for static content, like the css file in this example
@@ -15,11 +11,11 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public'))); 
 
-app.get('/', home.index);
-app.get('/directions', directions.index);
-app.get('/menu', menu.index);
-app.get('/menu/burgers', menu.burgers);
-app.get('/menu/sides', menu.sides);
-app.get('/menu/desserts', menu.desserts);
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/:viewname', function(req, res){
+    res.render(req.params.viewname);
+});
  
 app.listen(3000);
