da9253f5d2340114331a53c5f937b1a51bf4640c
Daniel Carapia <toonlink4943@gmail.com>
2017-02-15 08:54:54 -0700

    Made script work, wasn't being found in top level of directory so it's been moved to scripts/script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-exports.randomFortune = function() {
-    var fortunes = ["You're gunna die",
-    "You're gunna make monies",
-    "Something will happen today",
-    "Run! They're coming!",
-    "You're gunna live a very average life"];
-    var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    return fortune;
-}
