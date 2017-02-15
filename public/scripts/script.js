3cf5593090e562c371d0d03311a2a592c12d6543
Daniel Carapia <toonlink4943@gmail.com>
2017-02-15 09:52:26 -0700

    map is functional, working on different feature

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,39 @@
-console.log("Script active");
+function initMap(){
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var pos;
+    var uluru = {lat:40.768213, lng:-111.889179};
+
+    var map = new google.maps.Map(document.getElementById('map'),{
+    zoom:16,
+    center:uluru
+});
+
+    directionsDisplay.setMap(map);
+
+    var marker = new google.maps.Marker({
+        position:uluru,
+        map:map
+    });
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+        pos = {
+            lat:  position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        
+        map.setCenter(pos);
+        
+        });
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay){
+        directionsService.route({
+            origin: new google.maps.LatLng(pos.lat, pos.lng),
+            destination: uluru,
+            travelMode: 'DRIVING'
+        });
+    }
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
