d3c4914936483d66af56ce50e10393306136f78c
Maciej <maciejsmusz@gmail.com>
2017-02-17 22:23:39 -0700

    Changed the Background

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ app.get('/', function(req, res){
     res.render('index');
 });
 app.get('/:viewname', function(req, res){
-//<<<<<<< HEAD
     res.render(req.params.viewname, pureJson);
     res.status(404).send("Sorry, can't find that page!");
-// =======
-//     res.render(req.params.viewname);
-//     //res.status(404).send("Sorry, can't find that page!")
-// >>>>>>> 2eb6066d5907fef05efadf788f5d3c8c187faf41
 });
 
 app.listen(3000);
